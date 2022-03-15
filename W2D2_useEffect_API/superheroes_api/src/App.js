396dcfa67154aa269476e9b665f52bfc6bd128f9
jupiterorbita <38020888+jupiterorbita@users.noreply.github.com>
2022-03-14 19:43:54 -0700

    prepare w2d2 fetch-axios-useEffect lecture

---
+++ W2D2_useEffect_API/superheroes_api/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>🦸‍♀️🦸‍♀️ SuperHeroes 🦹‍♀️🦹‍♂️</h1>
+      <h4> fetch / Axios / useEffect()</h4>
+      <hr />
+      
+    </div>
+  );
+}
+
+export default App;
