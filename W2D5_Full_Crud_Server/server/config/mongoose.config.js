d34b25831aa3de8f560ee93657e185560db225f1
jupiterorbita <38020888+jupiterorbita@users.noreply.github.com>
2022-03-21 07:28:59 -0700

    refactor console.log

--- W2D5_Full_Crud_Server/server/config/mongoose.config.js
+++ W2D5_Full_Crud_Server/server/config/mongoose.config.js
@@ -6,5 +6,5 @@ mongoose.connect('mongodb://localhost/'+DB_NAME , {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-    .then(() => console.log(`Established a connection to the ${DB_NAME} database`))
+    .then(() => console.log(`🎈 Established a connection to the ${DB_NAME} database`))
     .catch(err => console.log('Something went wrong when connecting to the database ', err));
